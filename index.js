82354a6b373d54baf5d7f305037721a763a094e8
alekseidarafeichyk <alekseidarafeichyk@gmail.com>
2020-10-10 12:24:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+const app = express();
+const port = 3010;
+
+const nodemailer = require("nodemailer");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let smpt_login = process.env.SMTP_LOGIN;
+let smpt_password = process.env.SMTP_PASSWORD;
+
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',//smtp.gmail.com  //in place of service use host...
+    auth: {
+        user: smpt_login,
+        pass: smpt_password
+    },
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+app.post('/sendMessage', async (req, res) => {
+
+    let {name,email,number,message} = req.body
+
+    let info = await transporter.sendMail({
+        from: 'Alex', // sender address
+        to: smpt_login, // list of receivers
+        subject: "Portfolio message", // Subject line
+        html: `<b>Portfolio page </b>
+    <div>Name : ${name}</div>
+    <div>Email : ${email}</div>
+    <div>Number : ${number}</div>
+    <div>Message : ${message}</div>
+`, // html body
+    });
+    res.send('ok!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
